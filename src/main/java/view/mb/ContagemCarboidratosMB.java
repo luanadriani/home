edefeddb9452fcb170f53a071f40292a5cd112b6
luanadriani <luanadriani2011@gmail.com>
2017-12-18 16:36:53 -0200

    Inicio da tela contagem de carboidratos

---
+++ src/main/java/view/mb/ContagemCarboidratosMB.java
@@ -0,0 +1,10 @@
+package view.mb;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "contagemCarboidratosMB")
+@SessionScoped
+public class ContagemCarboidratosMB {
+	
+}
